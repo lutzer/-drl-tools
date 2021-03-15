d208dfdcb49317a930511cc2623cda0095c91b12
lutzer <in_the_fishtank@gmx.de>
2021-03-15 14:35:05 +0100

    filestream test now working

--- modules/server/src/index.ts
+++ modules/server/src/index.ts
@@ -4,8 +4,20 @@ import { Server as SocketServer } from "socket.io";
 import path from 'path'
 import fs from 'fs'
 import wav from 'wav'
+import speech, { SpeechClient } from '@google-cloud/speech';
+import { Readable } from 'stream'
 
 import { config } from './config'
+import Pumpify from 'pumpify';
+
+function convertFloat32ToInt16(buffer : any) {
+  var l = buffer.length;
+  var buf = new Int16Array(l);
+  while (l--) {
+    buf[l] = Math.min(1, buffer[l])*0x7FFF;
+  }
+  return buf.buffer;
+}
 
 function startServer() : Promise<{ server: HttpServer, port: Number}> {
   return new Promise( (resolve) => {
@@ -13,8 +25,77 @@ function startServer() : Promise<{ server: HttpServer, port: Number}> {
     const http = createServer(app)
     const io = new SocketServer(http)
 
+    // io.on('connection', (socket) => {
+    //   console.log('user connected')
+
+    //   const speechClient = new speech.SpeechClient();
+    //   const inputStream = new Readable();
+
+    //   // see https://github.com/googleapis/nodejs-speech/blob/master/protos/google/cloud/speech/v1p1beta1/cloud_speech.proto#L196
+    
+
+    //   // inputStream.pipe(recognizeStream)
+
+    //   // const filePath = path.resolve(__dirname, `../data/track_${socket.id}.wav`)
+
+    //   var recognizeStream : Pumpify | null
+
+    //   socket.on('audio/start', (msg) => {
+    //     console.log("start", msg)
+    //     const config = {
+    //       encoding: 1,
+    //       sampleRateHertz: msg.sampleRate,
+    //       languageCode: 'en-US',
+    //       audioChannelCount: 1
+    //     }
+    //     recognizeStream = speechClient.streamingRecognize({ config: config, interimResults: true })
+    //       .on('error', (err) => console.log(err))
+    //       .on('end', () => console.log('end') )
+    //       .on('data', data => { console.log(data) })
+    //   })
+
+    //   socket.on('audio/data', (data) => {
+    //     // console.log(data)
+    //     recognizeStream?.write(data)
+    //   })
+
+    //   socket.on('audio/stop', () => {
+    //     console.log('stop')
+    //     recognizeStream?.end()
+    //   })
+    // })
+
+
     io.on('connection', (socket) => {
       console.log('user connected')
+
+      const filePath = path.resolve(__dirname, `../data/track_${socket.id}.wav`)
+      var fileWriter : wav.FileWriter | undefined
+
+
+      socket.on('audio/start', (msg) => {
+        fileWriter = new wav.FileWriter(filePath, {
+          channels: 1,
+          sampleRate: msg.sampleRate,
+          bitDepth: msg.bitDepth
+        });
+        fileWriter.on('end', () => {
+          socket.emit('audio/received')
+          console.log('end')
+        })
+        console.log('start')
+      })
+
+      socket.on('audio/data', (data) => {
+        // var left = convertFloat32ToInt16(data)
+        fileWriter?.write(data)
+        console.log('data')
+      })
+
+      socket.on('audio/stop', () => {
+        fileWriter?.end()
+        console.log('stop')
+      })
     })
 
     app.get('/', (req, res) => {
@@ -30,7 +111,9 @@ function startServer() : Promise<{ server: HttpServer, port: Number}> {
 export { startServer }
 
 /*
- LINKS: 
+ LINKS:
+ https://cloud.google.com/speech-to-text/docs/streaming-recognize
+ https://subvisual.com/blog/posts/39-tutorial-html-audio-capture-streaming-to-node-js-no-browser-extensions/
  https://github.com/miguelgrinberg/socketio-examples/tree/master/audio
  https://blog.takeer.com/streaming-binary-data-using-socket-io/
  https://stackoverflow.com/questions/48627210/how-to-stream-data-over-socket-io-to-client
--- modules/server/test/send.spec.js
+++ modules/server/test/send.spec.js
@@ -22,19 +22,45 @@ describe('Stream Server Tests', () => {
         await server.close()
     })
 
+    async function connectSocket() {
+        return new Promise((resolve, reject) => {
+            const socket = io(address)
+            socket.on("connect", () => {
+                resolve(socket)
+            });
+            socket.on('error', () => {
+                reject()
+            })
+        })
+    }
+
     it('should connect to http server', async () => {
         const response = await axios.get(address)
         expect(response.status).to.equal(200)
     })
   
-    it('should connect to socket server', (done) => {
-        const socket = io(address)
-        socket.on("connect", () => {
-            done()
-        });
+    it('should connect to socket server', async () => {
+        const socket = await connectSocket()
+        expect(socket.connected).to.be.true
     })
 
-    it('should be able to stream file to server', async () => {
-        const file = __dirname + '/files/sound-wav.wav'
+    it.only('should be able to stream file to server', async () => {
+        const socket = await connectSocket()
+        const filepath = __dirname + '/files/sound-wav.wav'
+        const fileStream = fs.createReadStream(filepath, { highWaterMark: 8 * 1024 });
+        fileStream.on('open', function () {
+            socket.emit('audio/start', { sampleRate: 44100, bitDepth: 16 })
+        });
+        fileStream.on('data', (chunk) => {
+            socket.emit('audio/data', chunk)
+        })
+        fileStream.on('end',function() {
+            socket.emit('audio/stop')
+        });
+        return new Promise((resolve, reject) => {
+            socket.on('audio/received', () => {
+                resolve()
+            })
+        })
     })
 })
